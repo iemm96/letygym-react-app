f599260c1bba1081bc8dee024292d90b5cec6a83
Emmanuel <emmanuel@nucleodev.com>
2020-08-07 19:21:27 -0500

    ·Se actualiza asistencia a hook

--- src/Asistencia.js
+++ src/Asistencia.js
@@ -1,122 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from "react";
 import './styles/style.scss';
 import ListaResultados from "./ListaResultados";
 import ModalMembresia from "./ModalMembresia";
 import {url_base} from './constants/api_url';
+import {fetchRecords} from "./actions/fetchRecords";
 
 const api_url = url_base;
 
-let arr = ["anagrams-of-string-(with-duplicates)", "array-concatenation", "array-difference", "array-includes", "array-intersection", "array-remove", "array-sample", "array-union", "array-without", "array-zip", "average-of-array-of-numbers", "bottom-visible", "capitalize-first-letter-of-every-word", "capitalize-first-letter", "chain-asynchronous-functions", "check-for-palindrome", "chunk-array", "collatz-algorithm", "compact", "count-occurrences-of-a-value-in-array", "current-URL", "curry", "deep-flatten-array", "distance-between-two-points", "divisible-by-number", "drop-elements-in-array", "element-is-visible-in-viewport", "escape-regular-expression", "even-or-odd-number", "factorial", "fibonacci-array-generator", "fill-array", "filter-out-non-unique-values-in-an-array", "flatten-array-up-to-depth", "flatten-array", "get-days-difference-between-dates", "get-max-value-from-array", "get-min-value-from-array", "get-native-type-of-value", "get-scroll-position", "greatest-common-divisor-(GCD)", "group-by", "hamming-distance", "head-of-list", "hexcode-to-RGB", "initial-of-list", "initialize-array-with-range", "initialize-array-with-values", "is-array", "is-boolean", "is-function", "is-number", "is-string", "is-symbol", "last-of-list", "measure-time-taken-by-function", "median-of-array-of-numbers", "nth-element-of-array", "number-to-array-of-digits", "object-from-key-value-pairs", "object-to-key-value-pairs", "ordinal-suffix-of-number", "percentile", "pick", "pipe", "powerset", "promisify", "random-integer-in-range", "random-number-in-range", "redirect-to-URL", "reverse-a-string", "RGB-to-hexadecimal", "round-number-to-n-digits", "run-promises-in-series", "scroll-to-top", "shallow-clone-object", "shuffle-array", "similarity-between-arrays", "sleep", "sort-characters-in-string-(alphabetical)", "speech-synthesis-(experimental)", "standard-deviation", "sum-of-array-of-numbers", "swap-values-of-two-variables", "tail-of-list", "take-right", "take", "truncate-a-string", "unique-values-of-array", "URL-parameters", "UUID-generator", "validate-email", "validate-number", "value-or-default", "write-json-to-file"];
+const Asistencia = () => {
+    let arrayResults2 = [];
+    const [isHovering,setIsHovering] = useState(false);
+    const [arrayResults,setArrayResults] = useState([]);
+    const [arraySocias,setArraySocias] = useState([]);
+    const [isLoading,setIsLoading] = useState(false);
+    const [isDisabled,setIsDisabled] = useState(false);
+    const [isCorrect,setIsCorrect] = useState(false);
+    const [hoverEnable,setHoverEnable] = useState(false);
+    const [modalMembresia,setModalMembresia] = useState(false);
+    const [showList,setShowList] = useState(false);
+    const [buttonText,setButtonText] = useState('Registrar Asistencia');
 
-let list = '';
-let arrayResults = [];
-let masterResults = [];
-let arrayIdSocios = [];
-
-export default class Asistencia extends React.Component{
-
-    constructor(props)
-    {
-        super(props);
-        this.handleMouseHover = this.handleMouseHover.bind(this);
-        this.state = {
-            showResults: false,
-            isHovering: false,
-            arrayResults: [],
-            isLoading: false,
-            isCorrect: false,
-            isDisabled: false,
-            hoverEnable: true,
-            buttonText: 'Registrar Asistencia',
-            modalMembresia: false
-        }
-    }
-
-    componentDidMount() {
-
-        var arrNombres = [];
-        var arrSocios = [];
-
-        fetch(`${api_url}sociosyvisitantes`, {
-            // mode: 'no-cors',
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-            },
-        },)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-
-            }).then(response => {
-
-                response.map(item => {
-                    arrNombres.push(item.nombreCompleto);
-                    arrSocios.push(item.id);
-                });
-
-                this.setState({arrNombres: arrNombres, arrSocios: arrSocios});
+    useEffect(() => {
+        async function getSocias() {
+            try{
+                const result = await fetchRecords('sociosyvisitantes');
+                setArraySocias(result);
+            }catch (e) {
+                console.log(e);
             }
-        );
-        /*
-        var inputForm = document.getElementsByClassName('input-form');
 
-        inputForm.addEventListener('change', function () {
-            this.classList.remove('bounce');
-        });*/
-    }
-
-    updateResult = event => {
-        let resultList = document.querySelector("#result-list");
-
-        if(event.target.value.length > 2) {
-            arrayResults = [];
-
-            resultList.style = 'display: block';
-
-            var records = this.state.arrNombres;
-
-            records.map((val,index) => {
-
-                event.target.value.split(" ").map(word => {
-                    if(val.toLowerCase().indexOf(word.toLowerCase()) != -1) {
-
-                        //masterResults.push({nombre:val.nombreCompleto,bActiva:val.bActiva});
-                        arrayResults.push(val);
-                        arrayIdSocios.push(index);
-                        this.setState({arrayResults: arrayResults, arrayIdSocios: arrayIdSocios})
-                    }
-                });
-            });
-        }else{
-
-            resultList.style = 'display: none';
         }
 
-    };
+        getSocias();
+    },[]);
 
-    toggleModal = () => (this.state.modalMembresia ? this.setState({modalMembresia: false}) : this.setState({modalMembresia: true}));
+    const checarAsistencia = value => {
 
-    handleMouseHover = () => (this.setState(this.toogleHoverState));
-
-    toogleHoverState = state => {
-        return ({isHovering: !state.isHovering});
-    };
-
-    checarAsistencia = value => {
-
-        var state;
-        var self = this;
-
-        this.setState({hoverEnable: false});
-        this.setState({isLoading: true});
-        this.setState({buttonText: 'Registrando...'});
+        setHoverEnable(false);
+        setIsLoading(true);
+        setButtonText('Registrando...');
 
         fetch(`${api_url}socioMembresia/${value}`, {
-            // mode: 'no-cors',
             method: 'GET',
             headers: {
                 Accept: 'application/json',
@@ -133,120 +57,96 @@ export default class Asistencia extends React.Component{
 
                 if(response.bActiva) {
 
-                    self.setState({isLoading: false});
-                    self.setState({isCorrect: true});
-                    self.setState({isDisabled: true});
-                    self.setState({buttonText: 'Correcto!'});
+                    setIsLoading(false);
+                    setIsCorrect(true);
+                    setIsDisabled(false);
+                    
+                    setButtonText('¡Correcto!');
 
                     setTimeout(function () {
                         window.location.reload();
                     },5000);
                 }else{
-                    this.setState({modalMembresia: true})
+                    setModalMembresia( true);
                 }
             }
         );
 
     };
 
-    render() {
-        return (
-            <div className="row justify-content-center h-100">
-                <ModalMembresia
-                    toggleModal={this.toggleModal}
-                    modalMembresia={this.state.modalMembresia}
-                />
-                <div id="cardLogin" className="login card card-nav-tabs animate fadeInUp one">
-                    <div className="card-body">
-                        <form id="formLogin" className="center" onSubmit={this.submitForm}>
-                            <h3>Bienvenid@ a LetyGym</h3>
-                            <div className="form-group">
-                                <input onInput={this.updateResult}
-                                       id="inputNombre" type="text"
-                                       className="input-form" name="username"
-                                       aria-describedby=""
-                                       placeholder="Ingresa tu nombre para registrar tu asistencia"
-                                       onChange={this.clearInput}/>
-                            </div>
-                            <div className="">
-                                <ul id="result-list" className="list-group result">
-                                    <ListaResultados list={this.state.arrayResults}
-                                                     arrayIdSocios={this.state.arrSocios}
-                                                     checarAsistencia={this.checarAsistencia}
-                                                     isLoading={this.state.isLoading}
-                                                     isCorrect={this.state.isCorrect}
-                                                     isDisabled={this.state.isDisabled}
-                                                     buttonText={this.state.buttonText}
-                                                     hoverEnable={this.state.hoverEnable}
-                                    />
-                                </ul>
-                            </div>
+    const toggleModal = () => setModalMembresia(!modalMembresia);
+    
+    const handleInputChange = event => {
+        setArrayResults([]);
 
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+        //Si el string del campo es mayor a 2 se comienza a buscar en el arreglo
+        if(event.target.value.length > 1) {
 
-    submitForm(e) {
+            setShowList(true);
+            
+            //Se itera el arreglo de las socias para buscar el nombre
+            arraySocias.map((socia) => {
 
-        e.preventDefault();
-        var xhr = new XMLHttpRequest();
-        var inputNombre = document.getElementById('inputNombre');
-        var inputPassword = document.getElementById('inputPassword');
-        var cardLogin = document.getElementById('cardLogin');
+                let nombreCompleto = socia.nombreCompleto;
 
-        cardLogin.classList.remove('animate','one','fadeInUp');
+                event.target.value.split(' ').map(word => {
 
-        cardLogin.classList.add('fadeOut');
+                    //Se busca por nombre si se encuentra se regresa su id
+                    if(nombreCompleto.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+                        console.log('encontrado: ' , socia );
 
-        /*
-        // get a callback when the server responds
-        xhr.addEventListener('load', () => {
-            // update the state of the component with the result here
+                        arrayResults.push({nombre:socia.nombreCompleto,id:socia.id});
 
-            if(xhr.success !== true) {
-                if(xhr.type === 'password') {
-                    inputPassword.classList.add('bounce');
-                }
+                    }
+                });
 
-                if(xhr.type === 'email') {
-                    inputNombre.classList.add('bounce');
-                }
+            });
 
-                btnIngresar.innerText = 'Ingresar';
-            }
-            console.log(xhr.success)
-        });
-        // open the request with the verb and the url
-        xhr.open('POST', this.context)
-        // send the request
-        xhr.send()*/
+            console.log(arrayResults);
 
-        /*
-        if(inputPassword.innerText === '') {
-            inputPassword.classList.add('bounce');
+        }else{
+            setShowList(false);
         }
 
-        if(inputNombre.innerText === '') {
-            inputNombre.classList.add('bounce');
-        }*/
+    };
+    return (
+        <div className="row justify-content-center h-100">
+            <ModalMembresia
+                toggleModal={toggleModal}
+                modalMembresia={modalMembresia}
+            />
+            <div id="cardLogin" className="login card card-nav-tabs animate fadeInUp one">
+                <div className="card-body">
+                    <form id="formLogin" className="center">
+                        <h3>Bienvenid@ a LetyGym</h3>
+                        <div className="form-group">
+                            <input onInput={handleInputChange}
+                                   id="inputNombre" type="text"
+                                   className="input-form" name="username"
+                                   aria-describedby=""
+                                   placeholder="Ingresa tu nombre para registrar tu asistencia"
+                                   />
+                        </div>
+                        <div className="">
+                            <ul id="result-list" className="list-group result">
+                                {showList ? <ListaResultados list={arrayResults}
+                                                             checarAsistencia={checarAsistencia}
+                                                             isLoading={isLoading}
+                                                             isCorrect={isCorrect}
+                                                             isDisabled={isDisabled}
+                                                             buttonText={buttonText}
+                                                             hoverEnable={hoverEnable}
+                                                             showList={showList}
+                                /> : '' }
+                            </ul>
+                        </div>
 
-        //btnIngresar.innerText = 'Ingresar';
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
 
+export default Asistencia;
 
-        setTimeout(function () {
-            // and call `resolve` on the deferred object, once you're done
-            window.location.href = '/admin/index';
-
-        }, 500);
-
-    }
-
-    clearInput(e)
-    {
-        e.target.classList.remove('bounce');
-    }
-
-};
