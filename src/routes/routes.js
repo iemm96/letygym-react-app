5a9605ecbbfc0613d80926987a911d46a802f861
Emmanuel <emmanuel@nucleodev.com>
2020-08-14 19:36:21 -0500

    Se agrega el componente de notificaciones

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,15 +3,20 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from "../Login";
 import Dashboard from "../admin/Dashboard";
 import Asistencia from "../Asistencia";
+import ReactNotification from 'react-notifications-component'
 
 const AppRoutes = () =>
-    <BrowserRouter>
-        <Switch>
-            <Route path="/admin/index" component={Dashboard}/>
-            <Route path="/login" component={Login}/>
+    <div>
+        <ReactNotification/>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/admin/index" component={Dashboard}/>
+                <Route path="/login" component={Login}/>
 
-            <Route path="/" component={Asistencia}/>
-        </Switch>
-    </BrowserRouter>;
+                <Route path="/" component={Asistencia}/>
+            </Switch>
+        </BrowserRouter>
+    </div>
+    ;
 
 export default AppRoutes;
