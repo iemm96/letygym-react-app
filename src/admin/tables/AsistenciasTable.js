2961d9da6f5b15c721f8b3b993e9be1894490b73
Emmanuel <emmanuel@nucleodev.com>
2020-08-18 16:54:36 -0500

    Refactor de código

--- src/admin/tables/AsistenciasTable.js
+++ src/admin/tables/AsistenciasTable.js
@@ -1,50 +1,17 @@
 import React, {useEffect, useState} from "react";
-import ToolkitProvider, {Search} from "react-bootstrap-table2-toolkit";
-import BootstrapTable from "react-bootstrap-table-next";
-import paginationFactory, {PaginationListStandalone, PaginationProvider} from "react-bootstrap-table2-paginator";
-import {Button, Col, TabPane} from "reactstrap";
+import {Button, Col } from "reactstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBriefcase, faCheck, faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { Row } from "reactstrap";
-import EliminarRegistroModal from "../modals/EliminarRegistroModal";
-import {url_base} from '../../constants/api_url';
 import {fetchRecords} from "../../actions/fetchRecords";
 import MUIDataTable from "mui-datatables";
 import {muiTableOptions} from "../../constants/muiTableOptions";
-import ModalAsistencia from "../modals/ModalAsistencia";
 import Switch from "react-switch";
 import Select from "react-select";
-import {Icon} from "@material-ui/core";
-import ModalMembresia from "../../ModalMembresia";
 import RenovarMembresia from "../modals/RenovarMembresia";
 import {fetchRecord} from "../../actions/fetchRecord";
-import {updateRecord} from "../../actions/updateRecord";
 import moment from 'moment';
 import {storeRecord} from "../../actions/storeRecord";
-
-const { SearchBar } = Search;
-const api_url = url_base;
-const RESOURCE = 'asistencias';
-
-const Buscador = (props) => {
-    let input;
-    const search = () => {
-        props.onSearch(input.value);
-    };
-    return (
-        <Row className="row mb-2 justify-content-between">
-            <div className="col-3">
-                <input
-                    placeholder="Buscar asistencias..."
-                    className="form-control"
-                    ref={ n => input = n }
-                    type="text"
-                    onChange={search}
-                />
-            </div>
-        </Row>
-    );
-};
 
 const AsistenciasTable = props => {
     const [records,setRecords] = useState([]);
@@ -426,126 +393,3 @@ const AsistenciasTable = props => {
 };
 
 export default AsistenciasTable;
-
-/*
-class AsistenciasTable extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        state = {
-            asistencias: asistencias,
-            edit: false,
-            idRecord: null,
-            id_socio: '',
-            fechaHora: '',
-        };
-    }
-
-
-
-    toggleModal = () => {
-        modalRecord ? setState({modalRecord: false}) : setState({modalRecord: true});
-    };
-
-    toggleDeleteModal = () => {
-        deleteModal ? setState({deleteModal: false}) : setState({deleteModal: true});
-    }
-
-    prepareDeleteModal = (id,nombre) => {
-        setState({idRecord: id, nombre: nombre});
-
-        toggleDeleteModal();
-    }
-
-    deleteRegister = () => {
-        fetch(`${api_url}asistencias/${idRecord}`, {
-            method: 'DELETE',
-        }).then((res) => res)
-            .then((data) =>  {
-                if(data.ok) {
-                    window.location.reload();
-                }
-            })
-            .catch((err)=>console.log(err))
-    }
-
-
-    actionsFormatter = (cell, row) => (<div>
-        <Button type="Button" onClick={() => prepareDeleteModal(row.id, row.nombre)} className="btn btn-danger"><FontAwesomeIcon icon={faTrash} /></Button>
-    </div>);
-
-    render() {
-
-        const {error} = state;
-
-        if(error) {
-            alert(error.message);
-            return;
-        }
-
-
-
-        const options = {
-            custom: true,
-            paginationSize: 4,
-            pageStartIndex: 1,
-            firstPageText: 'Inicio',
-            prePageText: 'Atrás',
-            nextPageText: 'Siguiente',
-            lastPageText: 'Final',
-            nextPageTitle: 'Primer página',
-            prePageTitle: 'Página anterior',
-            firstPageTitle: 'Página siguiente',
-            lastPageTitle: 'Última página',
-            showTotal: true,
-            totalSize: asistencias.length
-        };
-
-        const contentTable = ({ paginationProps, paginationTableProps }) => (
-            <div>
-                <EliminarRegistroModal
-                    toggleDeleteModal={toggleDeleteModal}
-                    titulo={nombre}
-                    deleteRegister={deleteRegister}
-                    deleteModal={deleteModal}/>
-                <ToolkitProvider
-                    keyField="id"
-                    columns={ columns }
-                    data={ asistencias }
-                    search>
-                    {
-                        toolkitprops => (
-                            <div>
-                                <Buscador prepareNewModal={prepareNewModal} { ...toolkitprops.searchProps } />
-                                <BootstrapTable
-                                    hover
-                                    { ...toolkitprops.baseProps }
-                                    { ...paginationTableProps }
-                                />
-                            </div>
-                        )
-                    }
-                </ToolkitProvider>
-                <PaginationListStandalone { ...paginationProps } />
-            </div>
-        );
-
-        return(
-            <div>
-                <Col className="col-3">
-                </Col>
-                <PaginationProvider
-                    pagination={paginationFactory(options)}>
-
-                    {contentTable}
-
-                </PaginationProvider>
-            </div>
-
-        );
-    }
-
-}
-
-export default AsistenciasTable;*/
