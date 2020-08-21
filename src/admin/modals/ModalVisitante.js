7b67e5852be5810d304b965c5f06876baec4ffda
Emmanuel <emmanuel@nucleodev.com>
2020-08-20 23:24:18 -0500

    Se crea función de visitantes
    ·Se añade modal y tabla

--- src/admin/modals/ModalVisitante.js
+++ src/admin/modals/ModalVisitante.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     Modal,
     ModalHeader,
@@ -7,50 +7,177 @@ import {
     Button,
     Form,
     FormGroup,
-    Input,
+    input,
     Col,
     Row
 } from 'reactstrap';
+import {useForm} from "react-hook-form";
+import {fetchRecord} from "../../actions/fetchRecord";
+import {updateRecord} from "../../actions/updateRecord";
+import {store} from "react-notifications-component";
+import {storeRecord} from "../../actions/storeRecord";
 
-export default class ModalVisitante extends React.Component{
+const ModalVisita = props => {
+    const { register,errors, handleSubmit } = useForm();
+    const [record,setRecord] = useState(null);
 
-    render () {
-        return(<Modal isOpen={this.props.modalRecord} toggle={() => this.props.toggleModal(1)} className={this.props.className}>
-            <ModalHeader toggle={() => this.props.toggleModal(1)}>{this.props.editMode ? 'Editar' : 'Nueva'} Visitante</ModalHeader>
-            <ModalBody>
-                <Form id="form" onSubmit={this.props.editMode ? this.props.handleEditRecord : this.props.handleNewRecord}>
-                    <FormGroup>
-                        <Row form>
-                            <Col>
-                                <FormGroup>
-                                    <label>* Nombre</label>
-                                    <Input type="text" name="nombre" id="" value={this.props.editMode ? this.props.nombre : undefined}
-                                           onChange={event => this.props.handleInputChange(event)} />
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Row form>
-                            <Col md={6}>
-                                <label>* Apellido Paterno</label>
-                                <Input type="text" name="apellidoPaterno" id=""
-                                       value={this.props.editMode ? this.props.apellidoPaterno : undefined}
-                                       onChange={event => this.props.handleInputChange(event)} />
-                            </Col>
-                            <Col md={6}>
-                                <label>* Apellido Materno</label>
-                                <Input type="text" name="apellidoMaterno" id=""
-                                       value={this.props.editMode ? this.props.apellidoMaterno : undefined}
-                                       onChange={event => this.props.handleInputChange(event)} />
-                            </Col>
-                        </Row>
-                    </FormGroup>
-                </Form>
-            </ModalBody>
-            <ModalFooter>
-                <Button color="secondary" onClick={() => this.props.toggleModal()}>Cancelar</Button>
-                <Button form="form" type="submit" color="primary">{this.props.editMode ? 'Editar ' : 'Agregar '} Visitante</Button>
-            </ModalFooter>
-        </Modal>);
-    }
+    useEffect(() => {
+        async function getRecord() {
+            try {
+                const result = await fetchRecord(props.selectedRecordId,'visitantesVisitas');
+                if(result) {
+                    setRecord(result);
+                }
+            }catch (e) {
+                console.log(e);
+            }
+        }
+
+        if(props.selectedRecordId) {
+            getRecord();
+        }
+
+    },[props.selectedRecordId]);
+
+    const onSubmit = async (data) => {
+
+        if(record) {
+
+            try {
+
+                const response = await updateRecord(data,'visitantesVisitas',props.recordId);
+
+                if(response) {
+                    store.addNotification({
+                        title: "Correcto",
+                        message: "Se ha registrado una nueva visita",
+                        type: "success",
+                        insert: "top",
+                        container: "top-right",
+                        animationIn: ["animated", "fadeIn"],
+                        animationOut: ["animated", "fadeOut"],
+                        dismiss: {
+                            duration: 5000,
+                            onScreen: true
+                        }
+                    });
+                    setRecord(null);
+
+                    props.toggleModal();
+                    props.updateRecords();
+                }
+            }catch (e) {
+                console.log(e);
+                store.addNotification({
+                    title: "Ocurrió un error al registrar una visita",
+                    message: "Revisa tu conexión a internet",
+                    type: "danger",
+                    insert: "top",
+                    container: "top-right",
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 5000,
+                        onScreen: true
+                    }
+                });
+            }
+
+        }else{
+            try {
+                const response = await storeRecord(data,'visitantesVisitas');
+                if(response) {
+                    store.addNotification({
+                        title: "Correcto",
+                        message: "Se ha registrado una nueva visita",
+                        type: "success",
+                        insert: "top",
+                        container: "top-right",
+                        animationIn: ["animated", "fadeIn"],
+                        animationOut: ["animated", "fadeOut"],
+                        dismiss: {
+                            duration: 5000,
+                            onScreen: true
+                        }
+                    });
+                    props.toggleModal();
+                    props.updateRecords();
+                }
+
+            }catch (e) {
+                store.addNotification({
+                    title: "Ocurrió un error al registrar la visita",
+                    message: "Revisa tu conexión a internet",
+                    type: "danger",
+                    insert: "top",
+                    container: "top-right",
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 5000,
+                        onScreen: true
+                    }
+                });
+            }
+
+        }
+
+    };
+
+    return(<Modal isOpen={props.modalRecord} toggle={() => props.toggleModal(1)} className={props.className}>
+        <ModalHeader toggle={() => props.toggleModal(1)}>{record ? 'Editar' : 'Registrar'} Visita</ModalHeader>
+        <ModalBody>
+            <Form id="form" onSubmit={handleSubmit(onSubmit)}>
+                <FormGroup>
+                    <Row form>
+                        <Col>
+                            <FormGroup>
+                                <label>* Nombre</label>
+                                <input type="text"
+                                       name="nombre"
+                                       className="form-control"
+                                       defaultValue={record ? record.nombre : undefined}
+                                       ref={register({required:true})}
+                                />
+                                {errors.nombre && <small>Ingresa un nombre</small>}
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row form>
+                        <Col md={6}>
+                            <label>Apellido Paterno</label>
+                            <input type="text"
+                                   name="apellidoPaterno"
+                                   className="form-control"
+                                   defaultValue={record ? record.apellidoPaterno : undefined}
+                                   ref={register}
+                            />
+                            {errors.apellidoPaterno && <small>Ingresa un apellido paterno</small>}
+                        </Col>
+                        <Col md={6}>
+                            <label>Apellido Materno</label>
+                            <input type="text"
+                                   name="apellidoMaterno"
+                                   className="form-control"
+                                   defaultValue={record ? record.apellidoMaterno : undefined}
+                                   ref={register}
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col>
+                            <b>Cobrar: ${props.costoVisita}</b>
+                        </Col>
+                    </Row>
+                </FormGroup>
+            </Form>
+        </ModalBody>
+        <ModalFooter>
+            <Button color="secondary" onClick={() => props.toggleModal()}>Cancelar</Button>
+            <Button form="form" type="submit" color="primary">{record ? 'Editar ' : 'Registrar '} Visita</Button>
+        </ModalFooter>
+    </Modal>);
+
 };
 
+export default ModalVisita;
--- src/admin/tables/VisitantesTable.js
+++ src/admin/tables/VisitantesTable.js
@@ -1,324 +1,167 @@
-import React from "react";
-import ToolkitProvider, {Search} from "react-bootstrap-table2-toolkit";
-import BootstrapTable from "react-bootstrap-table-next";
-import paginationFactory, {PaginationListStandalone, PaginationProvider} from "react-bootstrap-table2-paginator";
-import {Button, Col, TabPane} from "reactstrap";
+import React, {useEffect, useState} from "react";
+import {Button, Col, InputGroup, InputGroupAddon, InputGroupText, Input} from "reactstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
-import RegistrarVenta from "../modals/RegistrarVenta";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
 import { Row } from "reactstrap";
-import EliminarRegistroModal from "../modals/EliminarRegistroModal";
-import {url_base} from '../../constants/api_url';
+import ModalEliminarRegistro from "../modals/ModalEliminarRegistro";
 import ModalVisitante from "../modals/ModalVisitante";
+import {deleteRecord} from "../../actions/deleteRecord";
+import {fetchRecords} from "../../actions/fetchRecords";
+import MUIDataTable from "mui-datatables";
+import {muiTableOptions} from "../../constants/muiTableOptions";
+import {updateRecord} from "../../actions/updateRecord";
+import {store} from "react-notifications-component";
 
-const { SearchBar } = Search;
-const api_url = url_base;
+const VisitantesTable = props => {
+    const [records,setRecords] = useState([]);
+    const [modalControl,setModalControl] = useState(false);
+    const [modalEliminar,setModalEliminar] = useState(false);
+    const [selectedRecordId,setSelectedRecordId] = useState(null);
+    const [tituloModal,setTituloModal] = useState('');
+    const [costoVisita,setCostoVisita] = useState(null);
+    const [isDisabled,setIsDisabled] = useState(true);
 
-let records = [{
+    useEffect(() => {
+        getRecords();
+    },[]);
 
-}];
+    const eliminarRegistro = async () => {
+        try {
 
-const Buscador = (props) => {
-    let input;
-    const search = () => {
-        props.onSearch(input.value);
-    };
-    return (
-        <Row className="row mb-2 justify-content-between">
-            <div className="col-3">
-                <input
-                    placeholder="Buscar Visitantes..."
-                    className="form-control"
-                    ref={ n => input = n }
-                    type="text"
-                    onChange={search}
-                />
-            </div>
-            <div className="col-2">
-                <Button className="actionButton" onClick={() => props.prepareNewModal()}>Nueva Visitante</Button>
-            </div>
-        </Row>
-    );
-};
+            setModalEliminar(false);
+            await deleteRecord(selectedRecordId,'socios');
 
-class VisitantesTable extends React.Component {
+            getRecords();
 
-    constructor(props) {
-        super(props);
+        }catch (e) {
 
-        this.state = {
-            records: records,
-            edit: false,
-            idRecord: null,
-            nombre: '',
-            apellidoPaterno: '',
-            apellidoMaterno: ''
-        };
-    }
-
-    componentDidMount() {
-
-        fetch(`${api_url}visitantesVisitas`, {
-            // mode: 'no-cors',
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-            },
-        },)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-
-            }).then(response =>
-            this.setState({records: response})
-        );
-    }
-
-    toggleModal = () => {
-        this.state.modalRecord ? this.setState({modalRecord: false}) : this.setState({modalRecord: true});
-    };
-
-    prepareNewModal = () => {
-        this.setState({edit: false});
-
-        this.toggleModal();
-    }
-
-    prepareEditModal = id => {
-        this.setState({edit: true,idRecord: id});
-
-        fetch(`${api_url}socios/${id}`, {
-            // mode: 'no-cors',
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-            },
-        },)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-
-            }).then(response => (this.setRecordData(response))
-        );
-
-        this.toggleModal();
-    }
-
-    setRecordData = data => {
-
-        this.setState({
-            ...data
-        })
-    };
-
-    toggleDeleteModal = () => {
-        this.state.deleteModal ? this.setState({deleteModal: false}) : this.setState({deleteModal: true});
-    }
-
-    handleInputChange = event => {
-
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleSelectChange = object => {
-        this.setState({
-            id_producto: object.value
-        });
-    }
-
-    getCurrentDateTime = () => {
-        var tempDate = new Date();
-        var date = tempDate.getFullYear() + '-' + (tempDate.getMonth()+1) + '-' + tempDate.getDate() +' '+ tempDate.getHours()+':'+ tempDate.getMinutes()+':'+ tempDate.getSeconds();
-        this.setState({fechaHora:date});
-
-    }
-
-    handleNewRecord = event => {
-
-        event.preventDefault();
-
-        this.getCurrentDateTime();
-
-        fetch(`${api_url}visitantesVisitas`, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json, text-plain, */*",
-            },
-            body:this.stringifyData()
-        }).then((res) => res.json())
-            .then((data) =>  console.log(data))
-            .catch((err)=>console.log(err))
-
-    }
-
-    stringifyData = () => {
-
-        var json = JSON.stringify({
-            nombre:this.state.nombre,
-            apellidoPaterno:this.state.apellidoPaterno,
-            apellidoMaterno:this.state.apellidoMaterno,
-        });
-
-        return json;
-    };
-
-    handleEditRecord = event => {
-
-        event.preventDefault();
-        fetch(`${api_url}socios/${this.state.idRecord}`, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json, text-plain, */*",
-            },
-            body:this.stringifyData()
-        }).then((res) => res.json())
-            .then((data) =>  console.log(data))
-            .catch((err)=>console.log(err))
-    }
-
-    prepareDeleteModal = (id,title) => {
-
-        this.setState({idRecord: id, title: title});
-
-        this.toggleDeleteModal();
-    }
-
-    deleteRegister = () => {
-        fetch(`${api_url}visitantesVisitas/${this.state.idRecord}`, {
-            method: 'DELETE',
-        }).then((res) => res)
-            .then((data) =>  {
-                if(data.ok) {
-                    //window.location.reload();
-                }
-            })
-            .catch((err)=>console.log(err))
-    }
-
-    updateTotal = total => {
-        this.setState({total:total})
-    }
-
-    actionsFormatter = (cell, row) => (<div>
-        <Button type="Button" onClick={() => this.prepareEditModal(row.id_socio)} className="btn mr-2 btn-primary"><FontAwesomeIcon icon={faEdit}/></Button>
-        <Button type="Button" onClick={() => this.prepareDeleteModal(row.id, row.nombre)} className="btn btn-danger"><FontAwesomeIcon icon={faTrash} /></Button>
-    </div>);
-
-    render() {
-
-        const {error} = this.state;
-
-        if(error) {
-            alert(error.message);
-            return;
         }
+    };
 
-        const columns = [{
-            dataField: 'nombreCompleto',
-            text: 'Nombre',
-            sort: true,
-        },
-            {
-                dataField: 'visitas',
-                text: 'Número de visitas',
-                sort: true,
-            }, {
-                dataField: 'actions',
-                text: 'Acciones',
-                isDummyField: true,
-                csvExport: false,
-                formatter: this.actionsFormatter,
-            },];
+    const getRecords = async () => {
+        try {
+            const result = await fetchRecords('visitantesVisitas');
+            if(result) {
+                setRecords(result);
+            }
+        }catch (e) {
+            console.log(e);
+        }
+    };
 
-        const options = {
-            custom: true,
-            paginationSize: 4,
-            pageStartIndex: 1,
-            firstPageText: 'Inicio',
-            prePageText: 'Atrás',
-            nextPageText: 'Siguiente',
-            lastPageText: 'Final',
-            nextPageTitle: 'Primer página',
-            prePageTitle: 'Página anterior',
-            firstPageTitle: 'Página siguiente',
-            lastPageTitle: 'Última página',
-            showTotal: true,
-            totalSize: records.length
-        };
+    const changeInputVisita = (event) => {
+        setIsDisabled(false);
+        setCostoVisita(event.target.value);
+    };
 
-        const contentTable = ({ paginationProps, paginationTableProps }) => (
-            <div>
-                <ModalVisitante
-                    toggleModal={this.toggleModal}
-                    handleNewRecord={this.handleNewRecord}
-                    handleEditRecord={this.handleEditRecord}
-                    handleInputChange={this.handleInputChange}
-                    handleSelectChange={this.handleSelectChange}
-                    modalRecord={this.state.modalRecord}
-                    idRecord={this.state.idRecord}
-                    nombre={this.state.nombre}
-                    apellidoPaterno={this.state.apellidoPaterno}
-                    apellidoMaterno={this.state.apellidoMaterno}
-                    editMode={this.state.edit}
-                    recordData={this.state}
-                    updateTotal={this.updateTotal}
-                />
-                <EliminarRegistroModal
-                    toggleDeleteModal={this.toggleDeleteModal}
-                    titulo={this.state.title}
-                    deleteRegister={this.deleteRegister}
-                    deleteModal={this.state.deleteModal}/>
-                <ToolkitProvider
-                    keyField="id"
-                    columns={ columns }
-                    data={ this.state.records }
-                    search>
-                    {
-                        toolkitprops => (
-                            <div>
-                                <Buscador prepareNewModal={this.prepareNewModal} { ...toolkitprops.searchProps } />
-                                <BootstrapTable
-                                    hover
-                                    { ...toolkitprops.baseProps }
-                                    { ...paginationTableProps }
-                                />
-                            </div>
-                        )
-                    }
-                </ToolkitProvider>
-                <PaginationListStandalone { ...paginationProps } />
-            </div>
-        );
+    const updateCostoVisita = async () => {
+          try {
+              const result = await updateRecord({costo_visita:costoVisita},'appStatus',1);
+              if(result) {
+                  store.addNotification({
+                      title: "Correcto",
+                      message: "Se actualizó el costo de la visita",
+                      type: "success",
+                      insert: "top",
+                      container: "top-right",
+                      animationIn: ["animated", "fadeIn"],
+                      animationOut: ["animated", "fadeOut"],
+                      dismiss: {
+                          duration: 5000,
+                          onScreen: true
+                      }
+                  });
+              }
+          }catch (e) {
+              console.log(e);
+          }
+    };
 
-        return(
-            <div>
-                <Col className="col-3">
+    const columns = [{
+        name: "nombreCompleto",
+        label: "Nombre",
+        options: {
+            filter: false,
+            sort: false,
+        }
+    },{
+        name: "visitas",
+        label: "Visitas",
+        options: {
+            filter: false,
+            sort: false,
+        }
+    },{
+        name: "id",
+        label: "Acciones",
+        options: {
+            filter: true,
+            sort: false,
+            empty: true,
+            customBodyRender: (value, tableMeta) => {
+                return (
+                    <div>
+                        <Button type="Button" onClick={() => {
+                            setSelectedRecordId(value);
+                            setModalEliminar(!modalEliminar);
+                            setTituloModal(tableMeta.rowData[1]);
+                        }} className="mr-2 btnAction"><FontAwesomeIcon icon={faTrash}/>
+                        </Button>
+                    </div>
+                );
+            }
+        }
+    }];
+
+    return(
+        <div>
+            {modalControl ? <ModalVisitante
+                costoVisita={props.costoVisita}
+                toggleModal={() => {setModalControl(!modalControl)}}
+                modalRecord={modalControl}
+                selectedRecordId={selectedRecordId}
+                updateRecords={() => getRecords()}
+                turnoActual={props.turnoActual}
+            /> : ''}
+            {modalEliminar ? <ModalEliminarRegistro
+                toggleDeleteModal={() => setModalEliminar(!modalEliminar)}
+                deleteModal={modalEliminar}
+                titulo={tituloModal}
+                deleteRegister={() => eliminarRegistro()}
+            /> : '' }
+
+            <Row className="justify-content-between">
+                <Col sm={2}>
+                    <InputGroup className="mt-2">
+                        <InputGroupAddon addonType="prepend">
+                            <InputGroupText>Visita $</InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="number" defaultValue={props.costoVisita} onChange={event => changeInputVisita(event)} />
+                        <InputGroupAddon addonType="append"><Button color="primary" disabled={isDisabled} onClick={() => updateCostoVisita()}>Actualizar</Button></InputGroupAddon>
+                    </InputGroup>
                 </Col>
-                <PaginationProvider
-                    pagination={paginationFactory(options)}>
+                <Col sm={2}>
+                    <Button
+                        className="actionButton"
+                        onClick={() => setModalControl(!modalControl)}
+                    >
+                        Registrar Visita
+                    </Button>
+                </Col>
+            </Row>
+            <Row className="mt-4">
+                <Col>
+                    <MUIDataTable
+                        title={"Visitantes"}
+                        data={records}
+                        columns={columns}
+                        options={muiTableOptions}
+                    />
+                </Col>
+            </Row>
+        </div>
 
-                    {contentTable}
-
-                </PaginationProvider>
-            </div>
-
-        );
-    }
-
-}
+    )
+};
 
 export default VisitantesTable;
