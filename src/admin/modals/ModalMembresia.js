d0d4f248511d95da5bbcb94a3ec88000a573901e
Emmanuel <emmanuel@nucleodev.com>
2020-08-20 21:23:14 -0500

    Se actualiza el modal de membresía y su tabla a hooks

--- src/admin/modals/ModalMembresia.js
+++ src/admin/modals/ModalMembresia.js
@@ -1,43 +1,109 @@
-import React, { useState } from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Form, FormGroup, Label, Input, FormText, Col } from 'reactstrap';
+import React, {useEffect, useState} from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Form, FormGroup, Label, input, FormText, Col } from 'reactstrap';
+import {fetchRecord} from "../../actions/fetchRecord";
+import {useForm} from "react-hook-form";
+import {storeRecord} from "../../actions/storeRecord";
+import {store} from "react-notifications-component";
+import {updateRecord} from "../../actions/updateRecord";
 
-export default class ModalProducto extends React.Component{
+const ModalMembresia = props => {
+    const { register,errors, handleSubmit } = useForm();
+    const [record,setRecord] = useState([]);
 
-    render () {
-        return(<Modal isOpen={this.props.modalRecord} toggle={() => this.props.toggleModal(1)} className={this.props.className}>
-            <ModalHeader toggle={() => this.props.toggleModal(1)}>{this.props.editMode ? 'Editar precio de' : 'Nueva'} Membresía</ModalHeader>
-            <ModalBody>
-                <Form id="form" onSubmit={this.props.editMode ? this.props.handleEditRecord : this.props.handleNewRecord}>
+    useEffect(() => {
+        async function getRecord() {
+            try {
+                const result = await fetchRecord(props.selectedRecordId,'membresias');
+                if(result) {
+                    setRecord(result);
+                }
+            }catch (e) {
+                console.log(e);
+            }
+        }
+
+        getRecord();
+    },[props.selectedRecordId]);
+
+    const onSubmit = async (data) => {
+
+        try {
+
+            const response = await updateRecord(data,'membresias',props.selectedRecordId);
+
+            if(response) {
+                store.addNotification({
+                    title: "Correcto",
+                    message: "Se ha actualizado la membresía",
+                    type: "success",
+                    insert: "top",
+                    container: "top-right",
+                    animationIn: ["animated", "fadeIn"],
+                    animationOut: ["animated", "fadeOut"],
+                    dismiss: {
+                        duration: 5000,
+                        onScreen: true
+                    }
+                });
+                props.toggleModal();
+                props.updateRecords();
+            }
+
+        }catch (e) {
+            store.addNotification({
+                title: "Ocurrió un error al actualizar la membresía",
+                message: "Revisa tu conexión a internet",
+                type: "danger",
+                insert: "top",
+                container: "top-right",
+                animationIn: ["animated", "fadeIn"],
+                animationOut: ["animated", "fadeOut"],
+                dismiss: {
+                    duration: 5000,
+                    onScreen: true
+                }
+            });
+        }
+    };
+
+    return(<Modal isOpen={props.modalRecord} toggle={() => props.toggleModal(1)} className={props.className}>
+        <ModalHeader toggle={() => props.toggleModal(1)}>Editar precio de Membresía</ModalHeader>
+        <ModalBody>
+            <Form id="form" onSubmit={handleSubmit(onSubmit)}>
                 <FormGroup>
-                        <Label>* Nombre</Label>
-                        <Input  name="membresia"
-                                value={this.props.editMode ? this.props.membresia : undefined}
-                                onChange={event => this.props.handleInputChange(event)}
-                                disabled
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>* Duración</Label>
-                        <Input name="duracion"
-                               value={this.props.editMode ? this.props.duracion : undefined}
-                               onChange={event => this.props.handleInputChange(event)}
-                               disabled
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>* Precio</Label>
-                        <Input name="precio"
-                               value={this.props.editMode ? this.props.precio : undefined}
-                               onChange={event => this.props.handleInputChange(event)}
-                        />
-                    </FormGroup>
-                </Form>
-            </ModalBody>
-            <ModalFooter>
-                <Button color="secondary" onClick={() => this.props.toggleModal}>Cancelar</Button>
-                <Button form="form" type="submit" color="primary">{this.props.editMode ? 'Editar' : 'Agregar '} Membresía</Button>
-            </ModalFooter>
-        </Modal>);
-    }
+                    <Label>* Nombre</Label>
+                    <input  name="membresia"
+                            className="form-control"
+                            defaultValue={record.membresia}
+                            disabled
+                            ref={register}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label>* Duración</Label>
+                    <input name="duracion"
+                           className="form-control"
+                           defaultValue={record.duracion}
+                           disabled
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label>* Precio</Label>
+                    <input name="precio"
+                           className="form-control"
+                           defaultValue={record.precio}
+                           ref={register({required:true})}
+                    />
+                    {errors.membresia && <small>Ingresa el monto de la mebresía</small>}
+                </FormGroup>
+            </Form>
+        </ModalBody>
+        <ModalFooter>
+            <Button color="secondary" onClick={() => props.toggleModal}>Cancelar</Button>
+            <Button form="form" type="submit" color="primary">Actualizar Membresía</Button>
+        </ModalFooter>
+    </Modal>);
 };
+
+export default ModalMembresia;
+
 
--- src/admin/tables/MembresiasTable.js
+++ src/admin/tables/MembresiasTable.js
@@ -1,312 +1,98 @@
-import React from "react";
-import ToolkitProvider, {Search} from "react-bootstrap-table2-toolkit";
-import BootstrapTable from "react-bootstrap-table-next";
-import paginationFactory, {PaginationListStandalone, PaginationProvider} from "react-bootstrap-table2-paginator";
-import {Button, Col, TabPane} from "reactstrap";
+import React, {useEffect, useState} from "react";
+import {Button, Col } from "reactstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
-import RegistrarVenta from "../modals/RegistrarVenta";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { Row } from "reactstrap";
-import EliminarRegistroModal from "../modals/EliminarRegistroModal";
-import {url_base} from '../../constants/api_url';
 import ModalMembresia from "../modals/ModalMembresia";
+import {fetchRecords} from "../../actions/fetchRecords";
+import MUIDataTable from "mui-datatables";
+import {muiTableOptions} from "../../constants/muiTableOptions";
 
-const { SearchBar } = Search;
-const api_url = url_base;
+const MembresiasTable = () => {
+    const [records,setRecords] = useState([]);
+    const [modalControl,setModalControl] = useState(false);
+    const [selectedRecordId,setSelectedRecordId] = useState(null);
+    const [tituloModal,setTituloModal] = useState('');
 
-let records = [{
+    useEffect(() => {
+        getRecords();
+    },[]);
 
-}];
-
-const Buscador = (props) => {
-    let input;
-    const search = () => {
-        props.onSearch(input.value);
-    };
-    return (
-        <Row className="row mb-2 justify-content-between">
-            <div className="col-3">
-                <input
-                    placeholder="Buscar Membresías..."
-                    className="form-control"
-                    ref={ n => input = n }
-                    type="text"
-                    onChange={search}
-                />
-            </div>
-            <div className="col-2">
-            </div>
-        </Row>
-    );
-};
-
-class MembresiasTable extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            records: records,
-            edit: false,
-            idRecord: null,
-            membresia: 0,
-            duracion: '',
-            precio: ''
-        };
-    }
-
-    componentDidMount() {
-
-        fetch(`${api_url}membresias`, {
-            // mode: 'no-cors',
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-            },
-        },)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-
-            }).then(response =>
-            this.setState({records: response})
-        );
-    }
-
-    toggleModal = () => {
-        this.state.modalRecord ? this.setState({modalRecord: false}) : this.setState({modalRecord: true});
-    };
-
-    prepareNewModal = () => {
-        this.setState({edit: false});
-
-        this.toggleModal();
-    }
-
-    prepareEditModal = id => {
-        this.setState({edit: true,idRecord: id});
-
-        fetch(`${api_url}membresias/${id}`, {
-            // mode: 'no-cors',
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-            },
-        },)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-
-            }).then(response => (this.setRecordData(response))
-        );
-
-        this.toggleModal();
-    }
-
-    setRecordData = data => {
-
-        this.setState({
-            ...data
-        })
-    };
-
-    toggleDeleteModal = () => {
-        this.state.deleteModal ? this.setState({deleteModal: false}) : this.setState({deleteModal: true});
-    }
-
-    handleInputChange = event => {
-
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleSelectChange = object => {
-        this.setState({
-            id_producto: object.value
-        });
-    }
-
-    getCurrentDateTime = () => {
-        var tempDate = new Date();
-        var date = tempDate.getFullYear() + '-' + (tempDate.getMonth()+1) + '-' + tempDate.getDate() +' '+ tempDate.getHours()+':'+ tempDate.getMinutes()+':'+ tempDate.getSeconds();
-        this.setState({fechaHora:date});
-
-    }
-
-    handleNewRecord = event => {
-
-        event.preventDefault();
-
-        this.getCurrentDateTime();
-
-        fetch(`${api_url}membresias`, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json, text-plain, */*",
-            },
-            body:this.stringifyData()
-        }).then((res) => res.json())
-            .then((data) =>  console.log(data))
-            .catch((err)=>console.log(err))
-    }
-
-    stringifyData = () => {
-
-        var json = JSON.stringify({
-            membresia:this.state.membresia,
-            duracion:this.state.duracion,
-            precio:this.state.precio,
-        });
-
-        return json;
-    };
-
-    handleEditRecord = event => {
-
-        event.preventDefault();
-
-        fetch(`${api_url}membresias/${this.state.idRecord}`, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json, text-plain, */*",
-            },
-            body:this.stringifyData()
-        }).then((res) => res.json())
-            .then((data) =>  {
-                if(data.id) {
-                    window.location.reload();
-                }
-            })
-            .catch((err)=>console.log(err))
-    }
-    
-    updateTotal = total => {
-        this.setState({total:total})
-    }
-
-    actionsFormatter = (cell, row) => (<div>
-        <Button type="Button" onClick={() => this.prepareEditModal(row.id)} className="btn mr-2 btn-primary"><FontAwesomeIcon icon={faEdit}/></Button>
-    </div>);
-
-    render() {
-
-        const {error} = this.state;
-
-        if(error) {
-            alert(error.message);
-            return;
+    const getRecords = async () => {
+        try {
+            const result = await fetchRecords('membresias');
+            if(result) {
+                setRecords(result);
+            }
+        }catch (e) {
+            console.log(e);
         }
+    };
 
-        const columns = [
-            {
-                dataField: 'membresia',
-                text: 'Nombre',
-                sort: true,
-            },
-            {
-                dataField: 'duracion',
-                text: 'Duración',
-                sort: true,
-            }, {
-                dataField: 'precio',
-                text: 'Precio',
-                sort: true,
-            },{
-                dataField: 'actions',
-                text: 'Acciones',
-                isDummyField: true,
-                csvExport: false,
-                formatter: this.actionsFormatter,
-            },];
+    const columns = [{
+        name: "membresia",
+        label: "Nombre",
+        options: {
+            filter: false,
+            sort: false,
+        }
+    },{
+        name: "duracion",
+        label: "Duración",
+        options: {
+            filter: false,
+            sort: false,
+        }
+    },{
+        name: "precio",
+        label: "Precio",
+        options: {
+            filter: false,
+            sort: false,
+        }
+    },{
+        name: "id",
+        label: "Acciones",
+        options: {
+            filter: true,
+            sort: false,
+            empty: true,
+            customBodyRender: (value, tableMeta) => {
+                return (
+                    <div>
+                        <Button type="Button" onClick={() => {
+                            setSelectedRecordId(value);
+                            setModalControl(!modalControl);
+                            setTituloModal(tableMeta.rowData[1]);
+                        }} className="mr-2 btnAction"><FontAwesomeIcon icon={faEdit}/></Button>
+                    </div>
+                );
+            }
+        }
+    }];
 
-        const options = {
-            custom: true,
-            paginationSize: 4,
-            pageStartIndex: 1,
-            firstPageText: 'Inicio',
-            prePageText: 'Atrás',
-            nextPageText: 'Siguiente',
-            lastPageText: 'Final',
-            nextPageTitle: 'Primer página',
-            prePageTitle: 'Página anterior',
-            firstPageTitle: 'Página siguiente',
-            lastPageTitle: 'Última página',
-            showTotal: true,
-            totalSize: records.length
-        };
-
-        const contentTable = ({ paginationProps, paginationTableProps }) => (
-            <div>
-                <ModalMembresia
-                    toggleModal={this.toggleModal}
-                    handleNewRecord={this.handleNewRecord}
-                    handleEditRecord={this.handleEditRecord}
-                    handleInputChange={this.handleInputChange}
-                    handleSelectChange={this.handleSelectChange}
-                    modalRecord={this.state.modalRecord}
-                    idRecord={this.state.idRecord}
-                    membresia={this.state.membresia}
-                    duracion={this.state.duracion}
-                    precio={this.state.precio}
-                    editMode={this.state.edit}
-                    recordData={this.state}
-                    updateTotal={this.updateTotal}
-                />
-                <EliminarRegistroModal
-                    toggleDeleteModal={this.toggleDeleteModal}
-                    titulo={this.state.title}
-                    deleteRegister={this.deleteRegister}
-                    deleteModal={this.state.deleteModal}/>
-                <ToolkitProvider
-                    keyField="id"
-                    columns={ columns }
-                    data={ this.state.records }
-                    search>
-                    {
-                        toolkitprops => (
-                            <div>
-                                <Buscador prepareNewModal={this.prepareNewModal} { ...toolkitprops.searchProps } />
-                                <BootstrapTable
-                                    hover
-                                    { ...toolkitprops.baseProps }
-                                    { ...paginationTableProps }
-                                />
-                            </div>
-                        )
-                    }
-                </ToolkitProvider>
-                <PaginationListStandalone { ...paginationProps } />
-            </div>
-        );
-
-        return(
-            <div>
-                <Col className="col-3">
+    return(
+        <div>
+            {modalControl ? <ModalMembresia
+                toggleModal={() => {setModalControl(!modalControl)}}
+                modalRecord={modalControl}
+                selectedRecordId={selectedRecordId}
+                tituloModal={tituloModal}
+                updateRecords={() => getRecords()}
+            /> : ''}
+            <Row className="mt-4">
+                <Col>
+                    <MUIDataTable
+                        title={"Membresías"}
+                        data={records}
+                        columns={columns}
+                        options={muiTableOptions}
+                    />
                 </Col>
-                <PaginationProvider
-                    pagination={paginationFactory(options)}>
+            </Row>
+        </div>
 
-                    {contentTable}
-
-                </PaginationProvider>
-            </div>
-
-        );
-    }
-
-}
+    )
+};
 
 export default MembresiasTable;
